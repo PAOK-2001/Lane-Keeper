0c30bcb734509002a25edb7476d9ad2f1638978c
PAOK-2001 <2001.paok@gmail.com>
2022-01-16 18:42:30 -0600

    Separated image preprocessing from class constructor, by adding loadFrame() method

--- laneDetector.cpp
+++ laneDetector.cpp
@@ -1,8 +1,6 @@
 #ifndef LANEDECTOR_CPP
 #define LANEDECTOR_CPP
 
-
-
 #include <bits/stdc++.h>
 #include "opencv2/core.hpp"
 #include "opencv2/face.hpp"
@@ -22,14 +20,17 @@ class laneDetector{
         Point center;
     public:
         laneDetector();
-        laneDetector(Mat cameraFeed);
+        void loadFrame(Mat cameraFrame);
         void findLanes();
         void display(Mat cameraFeed);
 
 
 };
 
-laneDetector::laneDetector(Mat cameraFeed){
+laneDetector::laneDetector(){
+}
+
+void laneDetector::loadFrame(Mat cameraFrame){
     // Image preprocessing code to load camera feed
         //1. Convert to grayscale
         //2. Gaussion blur with 5x5 kernel to facilitate edge detection
@@ -38,6 +39,7 @@ laneDetector::laneDetector(Mat cameraFeed){
         //Make mask using OpenCV poligon and aproximate coordinates based on lane width and camera FOV
         // Exclude Region of Interest by combining mask using bitwise_and operator
 }
+
 void laneDetector::findLanes(){
     // Find all lines in frame using HoughLinesP
     // Use polyfit to find intercepts and slopes of detected lines
