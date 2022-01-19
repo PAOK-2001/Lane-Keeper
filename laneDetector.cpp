e09abc1395bd41c18e77c32d563afbc1ed0a79b0
PAOK-2001 <2001.paok@gmail.com>
2022-01-18 22:24:12 -0600

    Detected average line for each side of lane, built line coordinates using equation.

--- laneDetector.cpp
+++ laneDetector.cpp
@@ -1,3 +1,6 @@
+#ifndef LANEDETECTOR_CPP
+#define LANEDETECTOR_CPP
+
 #include <bits/stdc++.h>
 #include "opencv2/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -9,20 +12,21 @@ using namespace cv;
 class laneDetector{
     private:
         // Region of intereset from where to detect lines
-        Mat frame;
+        Mat edgeImg, lineImg;
         // Lane lines in the format of a 4 integer vector such as  (x1, y1, x2, y2), which are the endpoint coordinates. 
-        Vec4i leftLine, righLine;
+        Vec4i leftLine, rightLine;
         // Coordinate that represents the senter of given lane
         Point center;
         // Auxiliary functions
-        static pair<double,double> linearFit(Vec4i lineCoordinates);
-        static double averageCoheficient(vector<double> registeredCoheficients);
+        static pair<float,float> linearFit(Vec4i lineCoordinates);
+        static float averageCoheficient(vector<float> registeredCoheficients);
+        static Vec4i makeCoordinate(float slope, float intercept, int imgHeight);
 
     public:
         laneDetector();
         void loadFrame(Mat cameraFrame);
         void findLanes();
-        void display();     
+        void display(Mat cameraFrame);
 };
 // Default constructor of lane detector
 laneDetector::laneDetector(){
@@ -30,15 +34,15 @@ laneDetector::laneDetector(){
 // Find slope (rise/run) and intercept (b=y-slope(x)) given end coordinates
 // @param Vec4i containing line end points (x1,y1,x2,y2)
 // @return Pair containing <slope,intercept>
-pair<double,double> laneDetector::linearFit(Vec4i lineCoordinates){
-    double slope     = (lineCoordinates[3] -lineCoordinates[1])/(lineCoordinates[2]-lineCoordinates[0]);
-    double intercept = lineCoordinates[1] - slope*lineCoordinates[0];
+pair<float,float> laneDetector::linearFit(Vec4i lineCoordinates){
+    float slope     = (lineCoordinates[3] -lineCoordinates[1])/(float)(lineCoordinates[2]-lineCoordinates[0]);
+    float intercept =  lineCoordinates[1] - slope*lineCoordinates[0];
     return  make_pair(slope,intercept);
 }
 // Find the average from a vector of cohedicientes
-// @param registerCoheficients Vector of doubles
-// @return avrg double representing average of vector. Retuns 0 if empty
-double laneDetector::averageCoheficient(vector<double> registeredCoheficients){
+// @param registerCoheficients Vector of floats
+// @return avrg float representing average of vector. Retuns 0 if empty
+float laneDetector::averageCoheficient(vector<float> registeredCoheficients){
     int size, sum;
     size = registeredCoheficients.size();
     if(size == 0){
@@ -47,42 +51,56 @@ double laneDetector::averageCoheficient(vector<double> registeredCoheficients){
     for(auto & num: registeredCoheficients){
         sum+=num;
     }
-    double avrg = sum/size;
+    float avrg = sum/size;
     return avrg;
+}
+// computes coordinate based on slope and intercpet of line equation
+// @param slope, intercept float representing linear equation values
+// @return coordinates Vec4i in format [x1,y1,x2,y2]
+Vec4i laneDetector::makeCoordinate(float slope, float intercept, int imgHeight){
+    int x1, y1, x2, y2;
+    y1 = imgHeight;
+    y2 = round(imgHeight*(2.0/5.0));
+    x1 = round((y1-intercept)/slope);
+    x2 = round((y2-intercept)/slope);
+    Vec4i returnCoordinates = {x1,y1,x2,y2};
+    return returnCoordinates;
 }
 // loads a frame and apply preprocessiong tecnques as well as masking to crop region of interest where lanes are
 // @param cameraFrame OpenCV image matrix
 void laneDetector::loadFrame(Mat cameraFrame){
     // Image preprocessing code to load camera feed
     // Saturate image
+    lineImg = cameraFrame.clone();
+    lineImg = Scalar(0,0,0);
     cameraFrame = cameraFrame*1.20;
     // Convert to grayscale
     Mat grayScale;
     cvtColor(cameraFrame,grayScale,COLOR_BGR2GRAY);
     // Canny edge detection
-    Canny(grayScale,frame,40,150);
+    Canny(grayScale,edgeImg,40,150);
 }
 void laneDetector::findLanes(){
     // Masking to exclude ROI
-    Mat mask = frame.clone();
+    Mat mask = edgeImg.clone();
     mask     = Scalar(0,0,0);
     // Make mask using OpenCV poligon and aproximate coordinates based on lane width and camera FOV
     // Create points for polygon
-    Point p1 = Point(10,frame.rows);
+    Point p1 = Point(10,mask.rows);
     Point p2 = Point(1120,650);
-    Point p3  = Point(1740,frame.rows);
+    Point p3  = Point(1740,mask.rows);
     vector<Point> ROI ={p1,p2,p3};
     fillPoly(mask,ROI,(255,255,255));
     // Exclude Region of Interest by combining mask using bitwise_and operator
-    bitwise_and(mask,frame,frame);
+    bitwise_and(mask,edgeImg,edgeImg);
     // Find all lines in frame using HoughLinesP
     vector<Vec4i> lines;
     // Uses HoughTransform to fine most probable line in canny image.
-    HoughLinesP(frame,lines,2,CV_PI/180,150,20);
+    HoughLinesP(edgeImg,lines,1,CV_PI/180,145,20);
     // Find regression for lines
-    vector<double> rightSide_slopes, rightSide_intercepts, leftSide_slopes,leftSide_intercepts;
-    for(auto &line: lines){
-        pair<double, double> fit =linearFit(line);
+    vector<float> rightSide_slopes, rightSide_intercepts, leftSide_slopes,leftSide_intercepts;
+    for(auto &lineP: lines){
+        pair<float, float> fit =linearFit(lineP);
         // Seperate left lane (negative slope)
         if(fit.first<0){
             leftSide_slopes.push_back(fit.first);
@@ -95,44 +113,21 @@ void laneDetector::findLanes(){
         }
     }
     // For each side, find average lane
-    double rightSlope     = averageCoheficient(rightSide_slopes);
-    double rightIntercept = averageCoheficient(rightSide_intercepts);
-    double leftSlope      = averageCoheficient(leftSide_slopes);
-    double leftIntercept  = averageCoheficient(leftSide_intercepts);
+    float rightSlope     = averageCoheficient(rightSide_slopes);
+    float rightIntercept = averageCoheficient(rightSide_intercepts);
+    float leftSlope      = averageCoheficient(leftSide_slopes);
+    float leftIntercept  = averageCoheficient(leftSide_intercepts);
     // Make coordinates for final lanes
+    leftLine  = makeCoordinate(leftSlope, leftIntercept, edgeImg.rows);
+    rightLine = makeCoordinate(rightSlope, rightIntercept, edgeImg.rows);
+    cout<<leftLine<<endl;
+    // Draw line on lineImg
+    line(lineImg,Point(leftLine[0],leftLine[1]),Point(leftLine[2],leftLine[3]),(255,0,0),5);
+}
+void laneDetector::display(Mat cameraFrame){
     
+    
+    imshow("Dash cam",lineImg);
 }
 
-void laneDetector::display(){
-    imshow("Dash cam",frame);
-}
-
-int main(){
-    laneDetector lanes;
-    // Create OpenCV frame object to store frame information
-    Mat frame;
-    // Create VideoCapture object
-    VideoCapture dashCam("Test Footage/Test2.mp4");
-    // Check if the dashCam is readable
-    if(!dashCam.isOpened()){
-        cout<<"Error reading dashCam feed\n";
-        return -1;
-    }
-    for (;;){
-        dashCam.read(frame);
-        // Check if selected device is sending information
-        if(frame.empty()){
-            cout<<"NULL frame ";
-            break;
-        }
-        lanes.loadFrame(frame);
-        lanes.findLanes();
-        lanes.display();
-        // Run face recognition function
-        // Read key board input, setting esc as break key
-        if(waitKey(5)== 27){
-            break;
-        }
-    }
-    return 0;
-}
+#endif
