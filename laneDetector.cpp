f3224e8c6a331e9e15a003ccadf0aa3e396b4237
PAOK-2001 <2001.paok@gmail.com>
2022-01-20 07:03:09 -0600

    Fixed bug to exclude outliers calculating average lane. As well as allow for colored lanes to be display.

--- laneDetector.cpp
+++ laneDetector.cpp
@@ -11,14 +11,14 @@ using namespace cv;
 
 class laneDetector{
     private:
-        // Region of intereset from where to detect lines
+        // Images states used in lane detection
         Mat edgeImg, lineImg;
         // Lane lines in the format of a 4 integer vector such as  (x1, y1, x2, y2), which are the endpoint coordinates. 
         Vec4i leftLine, rightLine;
         // Coordinate that represents the senter of given lane
         Point center;
         // Auxiliary functions
-        static pair<float,float> linearFit(Vec4i lineCoordinates);
+        static pair<float,float> linearFit(Vec4i lineCoordinates); 
         static float averageCoheficient(vector<float> registeredCoheficients);
         static Vec4i makeCoordinate(float slope, float intercept, int imgHeight);
 
@@ -28,6 +28,7 @@ class laneDetector{
         void findLanes();
         void display(Mat cameraFrame);
 };
+
 // Default constructor of lane detector
 laneDetector::laneDetector(){
 }
@@ -39,11 +40,12 @@ pair<float,float> laneDetector::linearFit(Vec4i lineCoordinates){
     float intercept =  lineCoordinates[1] - slope*lineCoordinates[0];
     return  make_pair(slope,intercept);
 }
+
 // Find the average from a vector of cohedicientes
 // @param registerCoheficients Vector of floats
 // @return avrg float representing average of vector. Retuns 0 if empty
 float laneDetector::averageCoheficient(vector<float> registeredCoheficients){
-    int size, sum;
+    float size, sum;
     size = registeredCoheficients.size();
     if(size == 0){
         return 0;
@@ -51,28 +53,33 @@ float laneDetector::averageCoheficient(vector<float> registeredCoheficients){
     for(auto & num: registeredCoheficients){
         sum+=num;
     }
-    float avrg = sum/size;
+    float avrg = sum/(float)size;
     return avrg;
 }
+
 // computes coordinate based on slope and intercpet of line equation
 // @param slope, intercept float representing linear equation values
 // @return coordinates Vec4i in format [x1,y1,x2,y2]
 Vec4i laneDetector::makeCoordinate(float slope, float intercept, int imgHeight){
     int x1, y1, x2, y2;
+    // Set default hight to bottom of image, assuming the hood of car isn't in frame
     y1 = imgHeight;
-    y2 = round(imgHeight*(2.0/5.0));
+    // Second height is arbitrary value, depending on how far we want to visualize lanes.
+    // #Note: there is a limit into how far the lane curvature holds.
+    y2 = round(imgHeight*(1.0/1.43));
+    // Calculate x coordinates solving in linea eq .y = mx+b---> x = y-b/m
     x1 = round((y1-intercept)/slope);
     x2 = round((y2-intercept)/slope);
+    // Build line coordinated structure
     Vec4i returnCoordinates = {x1,y1,x2,y2};
     return returnCoordinates;
 }
+
 // loads a frame and apply preprocessiong tecnques as well as masking to crop region of interest where lanes are
 // @param cameraFrame OpenCV image matrix
 void laneDetector::loadFrame(Mat cameraFrame){
     // Image preprocessing code to load camera feed
     // Saturate image
-    lineImg = cameraFrame.clone();
-    lineImg = Scalar(0,0,0);
     cameraFrame = cameraFrame*1.20;
     // Convert to grayscale
     Mat grayScale;
@@ -80,10 +87,14 @@ void laneDetector::loadFrame(Mat cameraFrame){
     // Canny edge detection
     Canny(grayScale,edgeImg,40,150);
 }
+
 void laneDetector::findLanes(){
     // Masking to exclude ROI
     Mat mask = edgeImg.clone();
     mask     = Scalar(0,0,0);
+    // Create canvas for line image, converting to BGR to allow for color lanes. 
+    lineImg  = mask;
+    cvtColor(lineImg,lineImg,COLOR_GRAY2BGR);
     // Make mask using OpenCV poligon and aproximate coordinates based on lane width and camera FOV
     // Create points for polygon
     Point p1 = Point(10,mask.rows);
@@ -95,14 +106,18 @@ void laneDetector::findLanes(){
     bitwise_and(mask,edgeImg,edgeImg);
     // Find all lines in frame using HoughLinesP
     vector<Vec4i> lines;
-    // Uses HoughTransform to fine most probable line in canny image.
-    HoughLinesP(edgeImg,lines,1,CV_PI/180,145,20);
+    // Uses HoughTransform to fine most lines in canny image.
+    HoughLinesP(edgeImg,lines,2,CV_PI/180,200,40,5);
     // Find regression for lines
     vector<float> rightSide_slopes, rightSide_intercepts, leftSide_slopes,leftSide_intercepts;
     for(auto &lineP: lines){
         pair<float, float> fit =linearFit(lineP);
+        // Exclude outliers.
+        if(fit.first == 0){
+            ;
+        }
         // Seperate left lane (negative slope)
-        if(fit.first<0){
+        else if(fit.first<0){
             leftSide_slopes.push_back(fit.first);
             leftSide_intercepts.push_back(fit.second);
         }
@@ -120,14 +135,17 @@ void laneDetector::findLanes(){
     // Make coordinates for final lanes
     leftLine  = makeCoordinate(leftSlope, leftIntercept, edgeImg.rows);
     rightLine = makeCoordinate(rightSlope, rightIntercept, edgeImg.rows);
-    cout<<leftLine<<endl;
     // Draw line on lineImg
-    line(lineImg,Point(leftLine[0],leftLine[1]),Point(leftLine[2],leftLine[3]),(255,0,0),5);
+    line(lineImg,Point(leftLine[0],leftLine[1]),Point(leftLine[2],leftLine[3]),Scalar(255,255,0),15);
+    line(lineImg,Point(rightLine[0],rightLine[1]),Point(rightLine[2],rightLine[3]),Scalar(0,255,0),15);
 }
+
+// Displays detected lanes unto a given image
+// @param frame to draw lanes unto.
 void laneDetector::display(Mat cameraFrame){
-    
-    
-    imshow("Dash cam",lineImg);
+    // Blend the lineImg of detected frame with camera feed for live visualization
+    addWeighted(cameraFrame,1,lineImg,0.4,0,cameraFrame);
+    imshow("Average lanes", cameraFrame);
 }
 
 #endif
